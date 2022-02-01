dcba5bee9e3b62cdb03440e370a58c94459e2dfe
Eduard Kutuev <ed@napych.com>
2022-02-01 18:06:30 +0300

    Moved Difra\Locales\Wordforms to difra/wordforms

---
+++ src/Wordforms/en_US.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Difra\Wordforms;
+
+/**
+ * Class en_US
+ * @package Difra\Wordforms
+ */
+class en_US extends Common
+{
+    /**
+     * Get quantity-based form
+     * @param string $word
+     * @param int $form
+     * @param int $quantity
+     * @return string
+     */
+    public function getQuantityForm(string $word, int $form, int $quantity): string
+    {
+        if ($quantity == 1) {
+            return $word;
+        } else {
+            $lastLetter = mb_substr($word, -1);
+            if ($lastLetter == 's') {
+                return mb_substr($word, 0, mb_strlen($word) - 1) . 'es';
+            } elseif ($lastLetter == 'y') {
+                return mb_substr($word, 0, mb_strlen($word) - 1) . 'ies';
+            } else {
+                return $word . 's';
+            }
+        }
+    }
+}
